1ea20c975bd6ebc3b2ba24bc4c589f95616efa19
Daniel Williams <danielw093@gmail.com>
2019-08-04 12:08:53 +0100

    Fixed warning

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import SiteLayout from './components/SiteLayout';
 import Home from './components/Home';
 import BlogHome from './components/Blog/BlogHome';
 import { Route } from 'react-router-dom';
-import BlogPost from './components/Blog/BlogPost/index';
+// import BlogPost from './components/Blog/BlogPost/index';
 
 const App: React.FC = () => {
   return (
