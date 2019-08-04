f5d2350619f5f9389749f0a41db524513aae7547
Daniel Williams <danielw093@gmail.com>
2019-08-04 11:07:00 +0000

    Feature/routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 //import styles from './App.module.scss';
 import SiteLayout from './components/SiteLayout';
+import Home from './components/Home';
 import BlogHome from './components/Blog/BlogHome';
+import { Route } from 'react-router-dom';
+import BlogPost from './components/Blog/BlogPost/index';
 
 const App: React.FC = () => {
   return (
     <SiteLayout>
-      <BlogHome />
+      <Route exact path="/" component={Home}/>
+      <Route path="/blog" component={BlogHome}/>
+      {/* <Route path="/blog/:id" component={BlogPost}/> */}
     </SiteLayout>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,17 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import iconLibraryInit from './utilities/icon-library';
+import { BrowserRouter } from 'react-router-dom';
 
 iconLibraryInit();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <App />
+    </BrowserRouter>
+);
+
+ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
