87a973228fb5f2db84221eb09a5dbc6eea098fef
Daniel Williams <danielw093@gmail.com>
2019-08-04 22:40:01 +0100

    Added navbar and footer

--- src/components/SiteLayout/index.tsx
+++ src/components/SiteLayout/index.tsx
@@ -1,17 +1,44 @@
 import React, { Component } from 'react';
 import styles from './index.module.scss';
+import { Link, NavLink } from 'react-router-dom';
+import { Navbar, Nav } from 'react-bootstrap';
 
 export default class SiteLayout extends Component {
-    render(){
+    render() {
         return (
-            <div>
-                <div className={styles.header}>
-                    
-                </div>
-                <div className={styles.contentWrapper}>
+            <div className={styles.content}>
+                {this.renderNavbar()}
+                <div className={styles.pageWrapper}>
                     {this.props.children}
                 </div>
+                {this.renderFooter()}
             </div>
         )
+    }
+
+    renderNavbar() {
+        return (
+            <Navbar bg="primary" variant="dark" expand="lg" className={styles.navbar}>
+                <Link to="">
+                    <Navbar.Brand>Candywool Games</Navbar.Brand>
+                </Link>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <NavLink exact to="/blog" className="nav-link">Blog</NavLink>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
+
+    renderFooter() {
+        return(
+            <Navbar bg="secondary" className={styles.footer}>
+                <div className={styles.footerText}>
+                    &copy; Copyright - Candywool Games 2019
+                </div>
+            </Navbar>
+        );
     }
 }
