8ad46abcff4c175a0e68cb89ad19135ea3d71be4
Daniel Williams <danielw093@gmail.com>
2019-08-07 14:16:50 +0100

    Improved page numbers slightly

--- src/components/PageNumbers/index.tsx
+++ src/components/PageNumbers/index.tsx
@@ -6,64 +6,89 @@ interface IPageNumbersProps {
     className?: string,
     currentPage: number,
     totalPages: number,
-    minimumPagesToDisplay: number;
     onUpdatePageNumber: (newPage: number) => void;
 }
 
-export default class PageNumbers extends Component<IPageNumbersProps, {}> {
-    static defaultProps = {
-        minimumPagesToDisplay: 5      
+interface IPageNumbersState {
+    numbers: number[];
+}
+
+export default class PageNumbers extends Component<IPageNumbersProps, IPageNumbersState> {
+    readonly pagesToDisplay = 5;
+
+    constructor(props: IPageNumbersProps){
+        super(props);
+
+        this.state = {
+            numbers: []
+        }
     }
 
-    render() {
-        let minimumPagesToDisplay = this.props.minimumPagesToDisplay;
-        let currentPage = this.props.currentPage;
-        let totalPages = this.props.totalPages;
+    componentDidMount() {
+        this.setNumberArray();
+    }
 
-        let items = [];
+    setNumberArray() {
+        //Feels like this method could be better
+        const pagesToDisplay = this.pagesToDisplay
+        const currentPage = this.props.currentPage;
+        const totalPages = this.props.totalPages;
 
-        if(minimumPagesToDisplay % 2 === 0){
-            minimumPagesToDisplay++;
-        }
-
-        const half = (minimumPagesToDisplay-1) / 2;
-
+        const half = (pagesToDisplay-1) / 2;
+        
         let firstPageToDisplay = currentPage-half;
         let lastPageToDisplay = currentPage+half;
-
+        
         if(firstPageToDisplay < 1){
             firstPageToDisplay = 1;
+            lastPageToDisplay = firstPageToDisplay + pagesToDisplay;
         }
 
-        if(lastPageToDisplay > totalPages) {
+        if (lastPageToDisplay > totalPages) {
             lastPageToDisplay = totalPages;
         }
 
-        for(let number = firstPageToDisplay; number <= lastPageToDisplay; number++){
-            items.push(
-                <Pagination.Item 
-                    key={number} 
-                    active={number === currentPage}
-                    onClick={this.pageChangeEvent}
-                >
-                    {number}
-                </Pagination.Item>
-            );
+        if(lastPageToDisplay - pagesToDisplay > 1) {
+            firstPageToDisplay = lastPageToDisplay - pagesToDisplay;
+        }
+        else {
+            firstPageToDisplay = 1;
         }
 
-        const isFirstPage = currentPage === 1;
-        const isLastPage = currentPage === totalPages;
+        let numbers : number[] = [];
+        for(let i = firstPageToDisplay; i <= lastPageToDisplay; i++){
+            numbers.push(i);
+        }
+
+        this.setState({ numbers:  numbers })
+    }
+
+    render() {
+        const isFirstPage = this.props.currentPage === 1;
+        const isLastPage = this.props.currentPage === this.props.totalPages;
         return (
             <div className={`${styles.wrapper} ${this.props.className}`}>
-                <Pagination>
+                <Pagination >
                     <Pagination.First onClick={this.pageChangeEvent} disabled={isFirstPage}/>
                     <Pagination.Prev onClick={this.pageChangeEvent} disabled={isFirstPage}/>
-                    {items}
+                    {this.state.numbers.map(this.renderPageNumber, this)}
                     <Pagination.Next onClick={this.pageChangeEvent} disabled={isLastPage}/>
                     <Pagination.Last onClick={this.pageChangeEvent} disabled={isLastPage}/>
                 </Pagination>
             </div>
         );
+    }
+
+    renderPageNumber(number: number) {
+        return (
+            <Pagination.Item 
+                key={number} 
+                active={number === this.props.currentPage}
+                onClick={this.pageChangeEvent}
+            >
+                {number}
+            </Pagination.Item>
+        )
     }
 
     pageChangeEvent = (event: React.MouseEvent) => {
